e1a14b9df96a23fef57562512844a6cebdcdceeb
Hermann Hueck <hermann.hueck@mailbox.org>
2020-06-28 19:21:47 +0200

    update diffx example

--- exploring-diffx/src/main/scala/github/DiffxReadmeExample.scala
+++ exploring-diffx/src/main/scala/github/DiffxReadmeExample.scala
@@ -10,6 +10,34 @@ import com.softwaremill.diffx._
  */
 object DiffxReadmeExample extends util.App {
 
+  {
+    sealed trait Parent
+    case class Bar(s: String, i: Int)                              extends Parent
+    case class Foo(bar: Bar, b: List[Int], parent: Option[Parent]) extends Parent
+
+    val right: Foo = Foo(
+      Bar("asdf", 5),
+      List(123, 1234),
+      Some(Bar("asdf", 5))
+    )
+    // right: Foo = Foo(Bar("asdf", 5), List(123, 1234), Some(Bar("asdf", 5)))
+
+    val left: Foo = Foo(
+      Bar("asdf", 66),
+      List(1234),
+      Some(right)
+    )
+    // left: Foo = Foo(
+    //   Bar("asdf", 66),
+    //   List(1234),
+    //   Some(Foo(Bar("asdf", 5), List(123, 1234), Some(Bar("asdf", 5))))
+    // )
+
+    import com.softwaremill.diffx._
+    compare(left, right) pipe println
+
+  }
+
   sealed trait Parent
   case class A(id: String, name: String) extends Parent
   case class B(id: String, name: String) extends Parent
@@ -32,6 +60,7 @@ object DiffxReadmeExample extends util.App {
 
   {
     s"${line(10)} diffing a1 and a2, ignoring field 'id':".green pipe println
+    @annotation.nowarn("msg=never used")
     implicit val diffA: Derived[Diff[A]] = Derived(Diff.gen[A].value.ignore((x: A) => x.id))
     Diff[Parent]
       .apply(a1, a2)
